20faacfad9e4a7e211d8c5b6bb4e4eda07b260a0
Nishant Modi <live.nm@gmail.com>
2024-02-01 00:44:53 +0100

    changes 3

---
+++ app/api/masks/route.tsx
@@ -0,0 +1,30 @@
+import { FrameRequest, getFrameAccountAddress, getFrameMessage } from '@coinbase/onchainkit';
+import { NextRequest, NextResponse } from 'next/server';
+
+async function getResponse(req: NextRequest): Promise<NextResponse> {
+  let accountAddress: string | undefined = '';
+  
+  const body: FrameRequest = await req.json();
+  const { isValid, message } = await getFrameMessage(body);
+
+  if (isValid) {
+    try {
+      accountAddress = await getFrameAccountAddress(message, { NEYNAR_API_KEY: 'NEYNAR_API_DOCS' });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  return new NextResponse(`<!DOCTYPE html><html><head>
+    <meta property="fc:frame" content="vNext" />
+    <meta property="fc:frame:image" content="/vercel.svg" />
+    <meta property="fc:frame:button:1" content="I want to save this" />
+    <meta property="fc:frame:post_url" content="/api/masks" />
+  </head></html>`);
+}
+
+export async function POST(req: NextRequest): Promise<Response> {
+  return getResponse(req);
+}
+
+export const dynamic = 'force-dynamic';
