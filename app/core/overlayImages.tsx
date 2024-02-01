3718e33845fea401a4f9389aacbc2f67c9aeadef
Nishant Modi <live.nm@gmail.com>
2024-02-01 21:47:30 +0100

    10

--- app/core/overlayImages.tsx
+++ app/core/overlayImages.tsx
@@ -1,8 +1,9 @@
 // utils/imageOverlay.ts
 
 import Jimp from 'jimp';
-import os from 'os';
-import path from 'path';
+import { useState } from "react";
+import { useS3Upload } from "next-s3-upload";
+import fs from 'fs';
 
 export interface OverlayOptions {
   x?: number; // X-coordinate of the overlay position (default: 0)
@@ -15,23 +16,28 @@ export async function overlayImages(baseImagePath: string, overlayImagePath: str
     const baseImage = await Jimp.read(baseImagePath);
     const overlayImage = await Jimp.read(overlayImagePath);
 
+    //s3 upload
+    let [imageUrl, setImageUrl] = useState();
+    let { FileInput, openFileDialog, uploadToS3 } = useS3Upload();
+
     // Set default options if not provided
     const { x = 0, y = 0 } = options;
 
     // Overlay the images at the specified coordinates
     baseImage.composite(overlayImage, x, y);
 
-    // Get the path to the temporary directory
-    const tmpDir = os.tmpdir();
+    // Save the composite image to a buffer
+    const buffer = await baseImage.getBufferAsync(Jimp.MIME_JPEG);
+    const blob = new Blob([buffer]);
+    const file = new File([blob], "name");
 
-    // Construct the output path in the temporary directory
-    const outputPath = path.join(tmpDir, outputFileName);
 
-    // Write the overlaid image to the temporary file
-    await baseImage.writeAsync(outputPath);
+    let { url } = await uploadToS3(file);
+    console.log(url);
+  
 
     // Return the path to the temporary file
-    return outputPath;
+    return url;
   } catch (error) {
     console.error('Error overlaying images:', error);
     throw new Error('Image overlay failed');
