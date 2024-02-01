36763fae9c8c8ea21b676c4ddec9d41105874788
Nishant Modi <live.nm@gmail.com>
2024-02-01 14:17:12 +0100

    10

--- app/pfp/page.tsx
+++ app/pfp/page.tsx
@@ -1,12 +1,18 @@
 'use client';
 import {useRouter} from "next/navigation";
 import {useEffect} from "react";
+import { useSearchParams } from 'next/navigation';
 
 export default function RedirectPage() {
     const router = useRouter();
+    const searchParams = useSearchParams();
+    let page: string;
+
+  // E.g. `/dashboard?page=2&order=asc`
+    page = searchParams.get('pfp') ?? 'https://metamask.io/developer/';
 
     useEffect(() => {
-        const metamaskurl = 'https://metamask.io/developer';
+        const metamaskurl = page;
 
         // Perform the redirect
         window.location.href = metamaskurl; // For a full page reload redirect
