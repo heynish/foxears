9f05d3af6f4b634c9ca57a53c7fd4aee82a95894
Nishant Modi <live.nm@gmail.com>
2024-02-01 23:05:24 +0100

    10

--- app/core/overlayImages.tsx
+++ app/core/overlayImages.tsx
@@ -11,27 +11,28 @@ export async function overlayImages(baseImagePath: string, overlayImagePath: str
     // Load base and overlay images using Jimp
     const baseImage = await Jimp.read(baseImagePath);
     const overlayImage = await Jimp.read(overlayImagePath);
-    const url = "";
 
     // Set default options if not provided
     const { x = 0, y = 0 } = options;
 
+
+    console.log('Calling composite');
     // Overlay the images at the specified coordinates
     baseImage.composite(overlayImage, x, y);
 
+    console.log('Calling buffer');
     // Save the composite image to a buffer
     const buffer = await baseImage.getBufferAsync(Jimp.MIME_PNG);
 
+    console.log('Calling upload');
     uploadToS3(buffer, "final").then(imageUrl => {
-      const url = imageUrl;
+      //Return URL
+      return imageUrl;
       console.log('The image has been uploaded. URL:', imageUrl);
     }).catch(error => {
       console.error('Error uploading to S3:', error);
     });
-  
-
-    // Return the path to the temporary file
-    return url;
+    return "https://mframes.vercel.app/2.png";
   } catch (error) {
     console.error('Error overlaying images:', error);
     throw new Error('Image overlay failed');
