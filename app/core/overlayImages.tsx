71d9eab76610a59b69471698e445f96e0a36be68
Nishant Modi <live.nm@gmail.com>
2024-02-02 19:11:38 +0100

    11

--- app/core/overlayImages.tsx
+++ app/core/overlayImages.tsx
@@ -24,8 +24,8 @@ export async function overlayImages(baseImagePath: string, overlayImagePath: str
     const overlayImage = await Jimp.read('https://mframes.vercel.app/ears.png');
     // Load face-api models
   //await faceapi.nets.tinyFaceDetector.loadFromDisk('/models');
-  await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
-  await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
+  await faceapi.nets.tinyFaceDetector.loadFromUri('https://mframes.vercel.app/models');
+  await faceapi.nets.faceLandmark68Net.loadFromUri('https://mframes.vercel.app/models');
   //await faceapi.nets.faceLandmark68Net.loadFromDisk('/models');
 
   // Scale down the picture (example: scale to 100x100)
