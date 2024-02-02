d5e17cad2fcd8cc20b0a537c351437d3bd88d15d
Nishant Modi <live.nm@gmail.com>
2024-02-02 17:00:16 +0100

    11

--- app/core/overlayImages.tsx
+++ app/core/overlayImages.tsx
@@ -34,10 +34,12 @@ export async function overlayImages(baseImagePath: string, overlayImagePath: str
 
      // Crop the image to the new size, from top-center
      picture.crop(0, 0, size, size);
+     console.log('picture.getWidth()', picture.getWidth());
+     console.log('picture.getHeight()', picture.getHeight());
 
     // Create a circle mask
     const diameter = picture.getWidth(); // assuming width & height are equal after resize
-    const mask = new Jimp(diameter, diameter, 0x037dd6); // start with a white circle on black bg
+    const mask = new Jimp(diameter, diameter, 0x0FFFFFF); // start with a white circle on black bg
     await mask.scan(0, 0, mask.getWidth(), mask.getHeight(), function(x, y, idx) {
       const distance = Math.sqrt(
         Math.pow(x - diameter / 2, 2) + Math.pow(y - diameter / 2, 2)
