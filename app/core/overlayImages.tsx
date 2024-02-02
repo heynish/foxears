0d7366b4e35501dd9c0ca3efb6148993098ed162
Nishant Modi <live.nm@gmail.com>
2024-02-02 16:35:15 +0100

    11

--- app/core/overlayImages.tsx
+++ app/core/overlayImages.tsx
@@ -3,7 +3,7 @@ import { uploadToS3 } from './uploadToS3';
 import crypto from 'crypto';
 import * as canvas from 'canvas';
 import * as faceapi from 'face-api.js';
-import * as tf from '@tensorflow/tfjs-node';
+//import * as tf from '@tensorflow/tfjs-node';
 const { Canvas, Image, ImageData } = canvas
 /*faceapi.env.monkeyPatch({
   Canvas,
