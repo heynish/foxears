288c7d783356ac819a755408198d14041180f36d
Nishant Modi <live.nm@gmail.com>
2024-02-03 20:31:31 +0100

    23

--- app/page.tsx
+++ app/page.tsx
@@ -28,17 +28,25 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <>
-      <img
-      src='https://mframes.vercel.app/1.png'
-      style={{
-      height: '100vh', // Viewport height
-      width: '100vw', // Viewport width
-      objectFit: 'cover', // Cover the viewport
-      position: 'absolute', // Position it over the page content
-      top: 0,
-      left: 0,
-      zIndex: -1 // Keep it behind other content
-    }}/>
+      <head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        {/* Add any other meta tags, title, or links needed here */}
+      </head>
+      <body>
+        {/* Existing content */}
+        <img
+          src='https://mframes.vercel.app/1.png'
+          style={{
+            height: '100vh', // Viewport height
+            width: '100vw', // Viewport width
+            objectFit: 'cover', // Cover the viewport
+            position: 'absolute', // Position it over the page content
+            top: 0,
+            left: 0,
+            zIndex: -1 // Keep it behind other content
+          }}
+        />
+      </body>
     </>
   );
 }
