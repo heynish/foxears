5d1fd1948327f4f8def457aa9eeeda82ab4a5b54
Nishant Modi <live.nm@gmail.com>
2024-02-03 13:19:36 +0100

    23

--- app/api/masks/route.tsx
+++ app/api/masks/route.tsx
@@ -86,7 +86,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
       //image: pfp+'.jpg',
       image: urlfinal,
       post_url: postURL,
-      refresh_period: 6,
+      refresh_period: 10,
     }),
   );
 /*
