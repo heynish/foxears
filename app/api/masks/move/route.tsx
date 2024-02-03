027193e4302c74466e230c608d24d4f398320246
Nishant Modi <live.nm@gmail.com>
2024-02-03 19:37:27 +0100

    23

--- app/api/masks/move/route.tsx
+++ app/api/masks/move/route.tsx
@@ -1,34 +1,30 @@
 import {
-    FrameRequest,
-    getFrameAccountAddress,
-    getFrameMessage,
-    getFrameHtmlResponse,
-  } from '@coinbase/onchainkit';
-  import { NextRequest, NextResponse } from 'next/server';
-  import { moveImage } from '../../../core/moveImage';
-  import ImageDetails from '../../../core/imageData';
-  
-  async function getResponse(req: NextRequest): Promise<NextResponse> {
-    // Get the URL object from the request
+  FrameRequest,
+  getFrameAccountAddress,
+  getFrameMessage,
+  getFrameHtmlResponse,
+} from '@coinbase/onchainkit';
+import { NextRequest, NextResponse } from 'next/server';
+import { moveImage } from '../../../core/moveImage';
+import ImageDetails from '../../../core/imageData';
+
+async function getResponse(req: NextRequest): Promise<NextResponse> {
+  // Get the URL object from the request
   const { searchParams } = req.nextUrl;
 
-    const response = await req.json();
-    const body: FrameRequest = response;
-    const { isValid, message } = await getFrameMessage(body);
-    if (isValid) {
-      try {
-           // Get the Farcaster ID from the message
-        //FID = message.fid ?? 3;
-        //accountAddress = await getFrameAccountAddress(message, { NEYNAR_API_KEY: 'NEYNAR_API_DOCS' });
-        //console.log(accountAddress);
-      } catch (err) {
-        console.error(err);
-      }
+  const response = await req.json();
+  const body: FrameRequest = response;
+  const { isValid, message } = await getFrameMessage(body);
+  if (isValid) {
+    try {
+    } catch (err) {
+      console.error(err);
     }
-    const buttonId = message?.buttonIndex || 0;
-    console.log('buttonId', buttonId);
- 
-       // Extracting individual query parameters
+  }
+  const buttonId = message?.buttonIndex || 0;
+  console.log('buttonId', buttonId);
+
+  // Extracting individual query parameters
   const urlParam = searchParams.get('url');
   const xParam = searchParams.get('x');
   const yParam = searchParams.get('y');
@@ -45,63 +41,55 @@ import {
   xFloat = parseFloat(xParam || '261.83333333333337');
   yFloat = parseFloat(yParam || '100.76666666666667');
 
-  if (buttonId == 1){
+  if (buttonId == 1) {
     xFloat = xFloat - 5;
-  } else if (buttonId == 2){
+  } else if (buttonId == 2) {
     xFloat = xFloat + 5;
-  } else if (buttonId == 3){
+  } else if (buttonId == 3) {
     yFloat = yFloat - 5;
-  } else if (buttonId == 4){
+  } else if (buttonId == 4) {
     yFloat = yFloat + 5;
   }
-  
 
-  const { urlfinal, urlbase, x, y }: ImageDetails =  await moveImage(urlBase, xFloat, yFloat, {
-      x: 50, // Set overlay position X-coordinate to 50
-      y: 50, // Set overlay position Y-coordinate to 50
-    });
 
-    const postURL = 'https://mframes.vercel.app/api/masks/move?url='+urlBase+'&x='+x+'&y='+y;
-    console.log(postURL);
+  const { urlfinal, urlbase, x, y }: ImageDetails = await moveImage(urlBase, xFloat, yFloat, {
+    x: 50, // Set overlay position X-coordinate to 50
+    y: 50, // Set overlay position Y-coordinate to 50
+  });
 
-    return new NextResponse(
-      getFrameHtmlResponse({
-        buttons: [
-          {
-            //label: `${accountAddress}`,
-            label: `◀️ Left`,
-          },
-          {
-            //label: `${accountAddress}`,
-            label: `Right ▶️`,
-          },
-          {
-            //label: `${accountAddress}`,
-            label: `🔼 Up`,
-          },
-          {
-            //label: `${accountAddress}`,
-            label: `🔽 Down`,
-          },
-        ],
-        image: urlfinal,
-        post_url: postURL,
-        refresh_period: 3,
-      }),
-    );
-  /*
-    return new NextResponse(`<!DOCTYPE html><html><head>
-      <meta property="fc:frame" content="vNext" />
-      <meta property="fc:frame:image" content="https://mframes.vercel.app/2.png" />
-      <meta property="fc:frame:button:1" content="${accountAddress}" />
-      <meta property="fc:frame:post_url" content="https://mframes.vercel.app/api/masks" />
-    </head></html>`);*/
-  }
-  
-  
-  export async function POST(req: NextRequest): Promise<Response> {
-    return getResponse(req);
-  }
-  
-  export const dynamic = 'force-dynamic';
-  
+  const postURL = 'https://mframes.vercel.app/api/masks/move?url=' + urlBase + '&x=' + x + '&y=' + y;
+  console.log(postURL);
+
+  return new NextResponse(
+    getFrameHtmlResponse({
+      buttons: [
+        {
+          //label: `${accountAddress}`,
+          label: `◀️ Left`,
+        },
+        {
+          //label: `${accountAddress}`,
+          label: `Right ▶️`,
+        },
+        {
+          //label: `${accountAddress}`,
+          label: `🔼 Up`,
+        },
+        {
+          //label: `${accountAddress}`,
+          label: `🔽 Down`,
+        },
+      ],
+      image: urlfinal,
+      post_url: postURL,
+      refresh_period: 3,
+    }),
+  );
+}
+
+
+export async function POST(req: NextRequest): Promise<Response> {
+  return getResponse(req);
+}
+
+export const dynamic = 'force-dynamic';
--- app/api/masks/route.tsx
+++ app/api/masks/route.tsx
@@ -22,14 +22,15 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   let outputimage: string | undefined = 'https://mframes.vercel.app/2.png';
   let accountPFP: string | undefined = '';
   let FID: number | undefined = 3;
-  
+
   const body: FrameRequest = await req.json();
   const { isValid, message } = await getFrameMessage(body);
   if (isValid) {
     try {
-         // Get the Farcaster ID from the message
+      // Get the Farcaster ID from the message
       FID = message.fid ?? 3;
-      accountAddress = await getFrameAccountAddress(message, { NEYNAR_API_KEY: 'NEYNAR_API_DOCS' });
+      //accountAddress = await getFrameAccountAddress(message, { NEYNAR_API_KEY: 'NEYNAR_API_DOCS' });
+      accountAddress = await getFrameAccountAddress(message, { NEYNAR_API_KEY: process.env.NEYNAR_API_KEY });
       console.log(accountAddress);
     } catch (err) {
       console.error(err);
@@ -37,58 +38,58 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   }
 
 
-    // Get username and pfp
-    const usernamePromise = fetch(
-        `${HUBBLE_URL}/userDataByFid?fid=${FID}&user_data_type=${USER_DATA_TYPE.USERNAME}`
-    );
-    const pfpPromise = fetch(
-        `${HUBBLE_URL}/userDataByFid?fid=${FID}&user_data_type=${USER_DATA_TYPE.PFP}`
-    );
-    const [usernameRes, pfpRes] = await Promise.all([usernamePromise, pfpPromise])
-    const usernameData: UserData = await usernameRes.json();
-    const username = usernameData.data.userDataBody.value;
-    const pfpData: UserData = await pfpRes.json();
-    const pfp = pfpData.data.userDataBody.value;
-    console.log(pfp);
+  // Get username and pfp
+  const usernamePromise = fetch(
+    `${HUBBLE_URL}/userDataByFid?fid=${FID}&user_data_type=${USER_DATA_TYPE.USERNAME}`
+  );
+  const pfpPromise = fetch(
+    `${HUBBLE_URL}/userDataByFid?fid=${FID}&user_data_type=${USER_DATA_TYPE.PFP}`
+  );
+  const [usernameRes, pfpRes] = await Promise.all([usernamePromise, pfpPromise])
+  const usernameData: UserData = await usernameRes.json();
+  const username = usernameData.data.userDataBody.value;
+  const pfpData: UserData = await pfpRes.json();
+  const pfp = pfpData.data.userDataBody.value;
+  console.log(pfp);
 
-    /*outputimage = await overlayImages('https://mframes.vercel.app/3.png', pfp+'.jpg', username+'.png', {
-      x: 50, // Set overlay position X-coordinate to 50
-      y: 50, // Set overlay position Y-coordinate to 50
-    });*/
+  /*outputimage = await overlayImages('https://mframes.vercel.app/3.png', pfp+'.jpg', username+'.png', {
+    x: 50, // Set overlay position X-coordinate to 50
+    y: 50, // Set overlay position Y-coordinate to 50
+  });*/
 
-    const { urlfinal, urlbase, x, y }: ImageDetails =  await overlayImages('https://mframes.vercel.app/3.png', pfp+'.jpg', username+'.png', {
-      x: 50, // Set overlay position X-coordinate to 50
-      y: 50, // Set overlay position Y-coordinate to 50
-    });
+  const { urlfinal, urlbase, x, y }: ImageDetails = await overlayImages('https://mframes.vercel.app/3.png', pfp + '.jpg', username + '.png', {
+    x: 50, // Set overlay position X-coordinate to 50
+    y: 50, // Set overlay position Y-coordinate to 50
+  });
 
-    //save to db
-    const userData = {
-      username: username,
-      address: accountAddress || "",
-      totalloads: 1,
-      lastimage: urlbase,
-    };
-    
-    // Add the user
-    async function checkUserAndPerformAction(username: string) {
-      try {
-        const totalLoads = await incrementUserTotalLoads(username);
-        if (totalLoads) {
-          console.log(`User exists.`);
-          // Perform actions based on the total loads
-        } else {
-          console.log('User does not exist.');
-          // Handle the case where the user does not exist
-          const newUser = await addUser(userData);
-          console.log('New User Added:', newUser);
-        }
-      } catch (error) {
-        console.error('An error occurred:', error);
+  //save to db
+  const userData = {
+    username: username,
+    address: accountAddress || "",
+    totalloads: 1,
+    lastimage: urlbase,
+  };
+
+  // Add the user
+  async function checkUserAndPerformAction(username: string) {
+    try {
+      const totalLoads = await incrementUserTotalLoads(username);
+      if (totalLoads) {
+        console.log(`User exists.`);
+        // Perform actions based on the total loads
+      } else {
+        console.log('User does not exist.');
+        // Handle the case where the user does not exist
+        const newUser = await addUser(userData);
+        console.log('New User Added:', newUser);
       }
+    } catch (error) {
+      console.error('An error occurred:', error);
     }
-    checkUserAndPerformAction(userData.username);
-    const postURL = 'https://mframes.vercel.app/api/masks/move?url='+urlbase+'&x='+x+'&y='+y;
-    console.log(postURL);
+  }
+  checkUserAndPerformAction(userData.username);
+  const postURL = 'https://mframes.vercel.app/api/masks/move?url=' + urlbase + '&x=' + x + '&y=' + y;
+  console.log(postURL);
   console.log('return response', postURL);
   return new NextResponse(
     getFrameHtmlResponse({
@@ -116,13 +117,13 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
       refresh_period: 10,
     }),
   );
-/*
-  return new NextResponse(`<!DOCTYPE html><html><head>
-    <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="https://mframes.vercel.app/2.png" />
-    <meta property="fc:frame:button:1" content="${accountAddress}" />
-    <meta property="fc:frame:post_url" content="https://mframes.vercel.app/api/masks" />
-  </head></html>`);*/
+  /*
+    return new NextResponse(`<!DOCTYPE html><html><head>
+      <meta property="fc:frame" content="vNext" />
+      <meta property="fc:frame:image" content="https://mframes.vercel.app/2.png" />
+      <meta property="fc:frame:button:1" content="${accountAddress}" />
+      <meta property="fc:frame:post_url" content="https://mframes.vercel.app/api/masks" />
+    </head></html>`);*/
 }
 
 
