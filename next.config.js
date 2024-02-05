a5070aba164e4afa590b33c0b3919e4af6396c8a
Nishant Modi <live.nm@gmail.com>
2024-02-05 13:58:54 +0100

    2.4

--- next.config.js
+++ next.config.js
@@ -1,15 +1,5 @@
 module.exports = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.externals = [...config.externals, "canvas", "jsdom"];
-    return config;
-  },
-}
-
-
-
-module.exports = {
-  reactStrictMode: true,
 };
 
 module.exports = {
