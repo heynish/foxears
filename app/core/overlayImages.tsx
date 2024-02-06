459c616b746908801baf1cc7c2dc6f9cb4a030c2
Nishant Modi <live.nm@gmail.com>
2024-02-06 14:26:40 +0100

    2.6

--- app/core/overlayImages.tsx
+++ app/core/overlayImages.tsx
@@ -13,6 +13,7 @@ export interface OverlayOptions {
 export async function overlayImages(baseImagePath: string, overlayImagePath: string, outputFileName: string, options: OverlayOptions = {}): Promise<ImageDetails> {
   try {
     // Load base and overlay images using Jimp
+    console.time('Image Processing Time 1');
     const baseImage = await Jimp.read(path.resolve('public/3.png'));
     const picture = await Jimp.read(overlayImagePath);
     const overlayImage = await Jimp.read(path.resolve('public/ears.png'));
@@ -58,12 +59,18 @@ export async function overlayImages(baseImagePath: string, overlayImagePath: str
       opacityDest: 1
     });
 
+
+
+
     //Upload base image
     const bufferbase = await baseImage.getBufferAsync(Jimp.MIME_JPEG);
     const newbufferbase = Buffer.from(bufferbase);
+    console.timeEnd('Image Processing Time 1');
+    console.time('Image Upload Time 1');
     const baseUrl = await uploadToS3(newbufferbase, crypto.randomBytes(7).toString('hex') + ".png");
-    //console.log('1.1.1 Base Image URL:', baseUrl);
+    console.timeEnd('Image Upload Time 1');
 
+    console.time('Image Processing Time 2');
     // Resize and position the overlay image at the top inside of the circle
     const overlayDiameter = 300 / 2.5; // Sizing the overlay as 1/3 of the circle's diameter
     overlayImage.resize(overlayDiameter, Jimp.AUTO); // Maintain aspect ratio
@@ -78,9 +85,11 @@ export async function overlayImages(baseImagePath: string, overlayImagePath: str
     // Save the composite image to a buffer
     const buffer = await baseImage.getBufferAsync(Jimp.MIME_PNG);
     const newbuffer = Buffer.from(buffer);
-
+    console.timeEnd('Image Processing Time 2');
     try {
+      console.time('Image Upload Time 2');
       const imageUrl = await uploadToS3(newbuffer, crypto.randomBytes(16).toString('hex') + "temp.png");
+      console.timeEnd('Image Upload Time 2');
       return { urlfinal: imageUrl, urlbase: baseUrl, x: overlayX, y: overlayY, w: overlayDiameter };
     } catch (error) {
       console.error('Error calling uploadToS3:', error);
