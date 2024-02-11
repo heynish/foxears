7d900f9556582806fc323f03783e735597d1d912
Nishant Modi <live.nm@gmail.com>
2024-02-11 21:54:34 +0100

    first

--- app/core/overlayImages.tsx
+++ app/core/overlayImages.tsx
@@ -5,36 +5,15 @@ import ImageDetails from '../core/imageData';
 import path from 'path';
 import sharp from 'sharp';
 
-export interface OverlayOptions {
-  x?: number; // X-coordinate of the overlay position (default: 0)
-  y?: number; // Y-coordinate of the overlay npm i canvas@2.6.1position (default: 0)
-}
-
-export async function overlayImages(baseImagePath: string, overlayImagePath: string, outputFileName: string, options: OverlayOptions = {}): Promise<ImageDetails> {
+export async function overlayImages(baseImagePath: string, overlayImagePath: string, outputFileName: string): Promise<ImageDetails> {
   try {
-    // Load base and overlay images using Jimp
-    console.time('Image Processing Time 1');
 
-    console.time('Image loading Time All');
-    const baseImage = sharp(path.resolve('public/3.png'));
-    const overlayImage = await Jimp.read(path.resolve('public/ears.png'));
     const picture = await Jimp.read(overlayImagePath);
-    console.timeEnd('Image loading Time All');
-
-    console.time('Image Resize Time');
-    // Scale down the picture (example: scale to 100x100)
     picture.resize(300, Jimp.AUTO);
-    // Determine the smallest dimension (width or height)
     const size = Math.min(picture.getWidth(), picture.getHeight());
-
-    // Crop the image to make it square from the top
     const croppedImage = picture.crop(0, 0, size, size);
-    console.timeEnd('Image Resize Time');
-
-    console.time('Image Masking Time');
-    // Create a circle mask with full transparency
     const diameter = croppedImage.getWidth();
-    const mask = new Jimp(diameter, diameter, 0x00000000); // Fully transparent
+    const mask = new Jimp(diameter, diameter, 0x00000000);
 
     // Draw a white circle on the mask
     mask.scan(0, 0, diameter, diameter, function (x, y, idx) {
@@ -50,57 +29,51 @@ export async function overlayImages(baseImagePath: string, overlayImagePath: str
       }
     });
 
-    const bufferbase = await picture.getBufferAsync(Jimp.MIME_PNG);
-    console.timeEnd('Image Masking Time');
+    // Apply the circle mask onto the picture to cut out the circular area
+    croppedImage.mask(mask, 0, 0);
 
-    console.time('Base Image Composition Time');
-    const { width: baseWidth = 0, height: baseHeight = 0 } = await baseImage.metadata();
+    // Create a new Jimp image with the specified dimensions and color
+    const background = new Jimp(800, 410, 0x000000ff);
 
-    const x = (baseWidth / 2) - (diameter / 2);
-    const y = (baseHeight / 2) - (diameter / 2);
+    // Calculate the position to center the circle on the base image
+    const x = (background.bitmap.width / 2) - (diameter / 2);
+    const y = (background.bitmap.height / 2) - (diameter / 2);
+    console.time('Image Composite Time');
+    // Composite the picture onto the base image at the calculated position
+    background.composite(croppedImage, x, y, {
+      mode: Jimp.BLEND_SOURCE_OVER,
+      opacitySource: 1,
+      opacityDest: 1
+    });
+    background.quality(50);
 
-    const compositeResult = await baseImage
-      .composite([{ input: bufferbase, top: y, left: x }])
-      .jpeg({ quality: 50 })
-      .toBuffer();
+    const bufferbase = await background.getBufferAsync(Jimp.MIME_JPEG);
+    const baseUrl = await uploadToS3(bufferbase, crypto.randomBytes(7).toString('hex') + ".png");
 
-    const newbufferbase = Buffer.from(compositeResult);
-    console.timeEnd('Base Image Composition Time');
 
-    const baseUrl = await uploadToS3(newbufferbase, crypto.randomBytes(7).toString('hex') + ".png");
-    console.log("baseUrl", baseUrl);
-
-    console.time('Ears Processing');
-    // Convert overlayImage to a buffer
-    let overlayBuffer = await overlayImage.getBufferAsync(Jimp.MIME_PNG);
+    const overlayImage = await Jimp.read(path.resolve('public/ears.png'));
 
     // Resize and position the overlay image at the top inside of the circle
     const overlayDiameter = 300 / 2.5; // Sizing the overlay as 1/3 of the circle's diameter
+    overlayImage.resize(overlayDiameter, Jimp.AUTO); // Maintain aspect ratio
 
     // Calculate the position for the top overlay
-    const overlayX = Math.round(x + (diameter - overlayDiameter) / 2); // Horizontally centered within the circle
-    const overlayY = Math.round(y + (diameter / 15)); // A little bit down from the top of the circle
-
-    // Resize overlayBuffer using Sharp
-    overlayBuffer = await sharp(overlayBuffer)
-      .resize({ width: overlayDiameter })
-      .png()
-      .toBuffer();
+    const overlayX = x + (diameter - overlayDiameter) / 2; // Horizontally centered within the circle
+    const overlayY = y + (diameter / 15); // A little bit down from the top of the circle
 
     // Composite the overlay image onto the base image
-    const finalBuffer = await sharp(newbufferbase)
-      .composite([{ input: overlayBuffer, left: overlayX, top: overlayY }])
-      .jpeg({ quality: 50 })
-      .toBuffer();
-    console.timeEnd('Ears Processing');
+    background.composite(overlayImage, overlayX, overlayY);
+    background.quality(50);
+    // Save the composite image to a buffer
+    const newbuffer = await background.getBufferAsync(Jimp.MIME_PNG);
 
     try {
-      const imageUrl = await uploadToS3(finalBuffer, crypto.randomBytes(16).toString('hex') + "temp.png");
+      const imageUrl = await uploadToS3(newbuffer, crypto.randomBytes(16).toString('hex') + "temp.png");
       return { urlfinal: imageUrl, urlbase: baseUrl, x: overlayX, y: overlayY, w: overlayDiameter };
     } catch (error) {
       console.error('Error calling uploadToS3:', error);
     }
-    return { urlfinal: "https://foxears.vercel.app/2.png", urlbase: "baseUrl", x: overlayX, y: overlayY, w: overlayDiameter };
+    return { urlfinal: "https://mframes.vercel.app/2.png", urlbase: "baseUrl", x: overlayX, y: overlayY, w: overlayDiameter };
   } catch (error) {
     console.error('Error overlaying images:', error);
     throw new Error('Image overlay failed');
